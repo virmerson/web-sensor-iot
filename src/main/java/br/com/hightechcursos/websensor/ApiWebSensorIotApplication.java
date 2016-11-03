ab50fc6ec2609dc176d5ef548903812c427d3144
virmerson <virmerson@hightechcursos.com.br>
2016-11-02 23:37:34 -0300

    First Commit - Defining initial structure

---
+++ src/main/java/br/com/hightechcursos/websensor/ApiWebSensorIotApplication.java
@@ -0,0 +1,12 @@
+package br.com.hightechcursos.websensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWebSensorIotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWebSensorIotApplication.class, args);
+	}
+}
